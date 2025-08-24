7b9a7ac111823905937950499741a35bc712d6cf
William2207 <122082110+William2207@users.noreply.github.com>
2025-08-24 21:17:53 +0700

    ProfileUI

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// Tạo một instance của Axios
+const api = axios.create({
+  baseURL: "http://localhost:5000/api", // Thay bằng baseURL của API
+});
+
+// Thêm một request interceptor
+api.interceptors.request.use(
+  (config) => {
+    // Lấy token từ localStorage
+    const token = localStorage.getItem("jwtToken");
+
+    // Nếu token tồn tại, thêm nó vào header Authorization
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    // Xử lý lỗi request
+    return Promise.reject(error);
+  }
+);
+
+export default api;
